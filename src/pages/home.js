fd03b00bfbcf05432f9a4a7c5f8979cf291c0677
lif3ng <lif3ng@icloud.com>
2021-08-27 13:24:03 +0800

    apollo client dynamic queries

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import NoteFeed from '/components/NoteFeed';
 import Button from '../components/Button';
+
+const GET_NOTES = gql`
+  query noteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`;
+
 const Home = () => {
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error!</p>;
+
   return (
-    <div>
-      <p>home page</p>
-      <Button>click me</Button>
-    </div>
+    <React.Fragment>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: data.noteFeed.cursor
+              },
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                const {
+                  noteFeed: { cursor, hasNextPage, notes }
+                } = fetchMoreResult;
+                return {
+                  noteFeed: {
+                    cursor,
+                    hasNextPage,
+                    notes: [...previousResult.noteFeed.notes, ...notes]
+                  }
+                };
+              }
+            })
+          }
+        >
+          Load more
+        </Button>
+      )}
+    </React.Fragment>
   );
 };
 
