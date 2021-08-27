4cde0ad4190f6a3a063f0bc45000a7a3312ec946
lif3ng <lif3ng@icloud.com>
2021-08-27 17:53:58 +0800

    auth and state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 import GlobalStyle from '/components/GlobalStyle';
 import Pages from './pages';
 
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
 const client = new ApolloClient({
-  uri,
+  link: authLink.concat(httpLink),
   cache,
+  resolvers: {},
   connectToDevTools: true
 });
-
+// init isLoggedIn store
+const data = { isLoggedIn: !!localStorage.getItem('token') };
+cache.writeData({ data });
+client.onResetStore(() => cache.writeData({ data }));
 const App = () => {
   return (
     <ApolloProvider client={client}>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import logo from '../img/logo.svg';
 
+import logo from '../img/logo.svg';
+import ButtonAsLink from './ButtonAsLink';
+
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 const HeaderBar = styled.header`
   width: 100%;
   padding: 0.5em 1em;
@@ -20,13 +29,37 @@ const LogoText = styled.h1`
   display: inline;
 `;
 
-const Header = () => {
+const UserState = styled.div`
+  margin-left: auto;
+`;
+
+const Header = props => {
+  const { data, client } = useQuery(IS_LOGGED_IN);
+
   return (
     <HeaderBar>
       <img src={logo} alt="Notedly Logo" height="40" />
       <LogoText>Notedly</LogoText>
+      <UserState>
+        {data.isLoggedIn ? (
+          <ButtonAsLink
+            onClick={() => {
+              localStorage.removeItem('token');
+              client.resetStore();
+              client.writeData({ data: { isLoggedIn: false } });
+              props.history.push('/');
+            }}
+          >
+            Log out
+          </ButtonAsLink>
+        ) : (
+          <p>
+            <Link to="/signin">Sign In</Link> <Link to="/signup">Sign Up</Link>
+          </p>
+        )}
+      </UserState>
     </HeaderBar>
   );
 };
 
-export default Header;
+export default withRouter(Header);
